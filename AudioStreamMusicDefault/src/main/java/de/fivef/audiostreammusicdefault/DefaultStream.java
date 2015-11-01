16c30a3757c47824f46700eb2d9c76fdb5a3f005
fivef <fivef2@gmail.com>
2015-11-01 16:39:40 +0100

    initial commit

---
+++ AudioStreamMusicDefault/src/main/java/de/fivef/audiostreammusicdefault/DefaultStream.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2014  FiveF
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.fivef.audiostreammusicdefault;
+
+import android.content.res.XResources;
+import android.media.AudioManager;
+import android.os.Build;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+
+/**
+ * Changes the default stream to the music stream.
+ * This class contains the code to be executed by Xposed.
+ *
+ * Based on the Volumesteps Xposed package by P1ngu1n
+ */
+public class DefaultStream implements IXposedHookZygoteInit {
+    private static final String LOG_TAG = "AudioStreamMusicDefault";
+
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        // Load the user's preferences
+        final XSharedPreferences prefs = new XSharedPreferences(BuildConfig.APPLICATION_ID);
+        final boolean debugging = prefs.getBoolean("pref_debug", false);
+        final boolean compatibilityModeLG = prefs.getBoolean("pref_compatibility_mode_lg", false);
+
+        if (debugging) {
+            XposedBridge.log(LOG_TAG + "Android " + Build.VERSION.RELEASE + " (SDK " + Build.VERSION.SDK_INT + ")");
+
+            Map<String, ?> sortedKeys = new TreeMap<String, Object>(prefs.getAll());
+            for (Map.Entry<String, ?> entry : sortedKeys.entrySet()) {
+                XposedBridge.log(LOG_TAG + entry.getKey() + "=" + entry.getValue().toString());
+            }
+
+            if (compatibilityModeLG) {
+                XposedBridge.log(LOG_TAG + "Using LG compatibility mode");
+            }
+        }
+
+        final Class<?> audioServiceClass = XposedHelpers.findClass(compatibilityModeLG ? "android.media.AudioServiceEx" : "android.media.AudioService", null);
+        final Class<?> audioSystemClass = XposedHelpers.findClass("android.media.AudioSystem", null);
+        final String maxStreamVolumeField = (compatibilityModeLG ? "MAX_STREAM_VOLUME_Ex" : "MAX_STREAM_VOLUME");
+
+
+        // Whether the volume keys control the music stream or the ringer volume
+        boolean volumeKeysControlMusic = prefs.getBoolean("pref_volume_keys_control_music", false);
+        if (debugging) XposedBridge.log(LOG_TAG + "Volume keys control " + (volumeKeysControlMusic ? "music" : "ringer"));
+
+        if (volumeKeysControlMusic) {
+            XposedHelpers.findAndHookMethod(audioServiceClass, "getActiveStreamType", int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    //mVoiceCapable is no longer available in Lollipop
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+                        boolean voiceCapable = XposedHelpers.getBooleanField(param.thisObject, "mVoiceCapable");
+                        if (!voiceCapable) return;
+                    }
+
+
+                    boolean isInCommunication = (Boolean) XposedHelpers.callMethod(param.thisObject, "isInCommunication");
+                    if (isInCommunication) return;
+
+                    int suggestedStreamType = (Integer) param.args[0];
+                    if (suggestedStreamType != AudioManager.USE_DEFAULT_STREAM_TYPE) return;
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        boolean isAfMusicActiveRecently = (Boolean) XposedHelpers.callMethod(param.thisObject, "isAfMusicActiveRecently", 5000);
+                        if (isAfMusicActiveRecently) return;
+                    } else {
+                        boolean musicStreamActive = (Boolean) XposedHelpers.callStaticMethod(audioSystemClass, "isStreamActive", AudioManager.STREAM_MUSIC, 5000);
+                        if (musicStreamActive) return;
+                    }
+
+                    // 4.4 and higher call checkUpdateRemoteStateIfActive at the MediaFocusControl class instead of AudioService
+                    Object objContainingRemoteStreamMethod = param.thisObject;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        objContainingRemoteStreamMethod = XposedHelpers.getObjectField(param.thisObject, "mMediaFocusControl");
+                    }
+                    boolean activeRemoteStream = (Boolean) XposedHelpers.callMethod(objContainingRemoteStreamMethod, "checkUpdateRemoteStateIfActive", AudioManager.STREAM_MUSIC);
+                    if (activeRemoteStream) return;
+
+                    param.setResult(AudioManager.STREAM_MUSIC);
+                    if (debugging) XposedBridge.log(LOG_TAG + "Event: intercepted getActiveStreamType call; returned STREAM_MUSIC");
+                }
+            });
+        }
+    }
+}
---
+++ AudioStreamMusicDefault/src/main/java/de/fivef/audiostreammusicdefault/SettingsFragment.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2014  FiveF
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.fivef.audiostreammusicdefault;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.view.View;
+import android.widget.CheckBox;
+
+/**
+ * Fragment container the preferences.
+ */
+public class SettingsFragment extends PreferenceFragment {
+    private static boolean rebootMessageShown = false;
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
+        addPreferencesFromResource(R.xml.settings);
+
+        // Set the version number in the about screen
+        findPreference("pref_about").setTitle(getString(R.string.pref_about_title, BuildConfig.VERSION_NAME));
+        // Set change listener to the 'show in launcher' preference
+        findPreference("pref_launcher").setOnPreferenceChangeListener(changeListenerLauncher);
+
+        // Volume keys control music requires Android 4.1
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            CheckBoxPreference volumeKeysControlMusic = (CheckBoxPreference) findPreference("pref_volume_keys_control_music");
+            volumeKeysControlMusic.setEnabled(false);
+            volumeKeysControlMusic.setSummaryOff(getString(R.string.require_android_Version, "Android 4.1"));
+        }
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        if (!rebootMessageShown && sharedPreferences.getBoolean("pref_show_reboot_dialog", true)) {
+            rebootMessageShown = true;
+            createRebootDialog().show();
+        }
+    }
+
+    /**
+     * Hides or shows the icon in the launcher when the preference changed.
+     */
+    private final Preference.OnPreferenceChangeListener changeListenerLauncher = new Preference.OnPreferenceChangeListener() {
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            int componentState = ((Boolean) newValue ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED);
+
+            Activity activity = getActivity();
+            ComponentName alias = new ComponentName(activity, "de.fivef.audiostreammusicdefault.SettingsActivity-Alias");
+            activity.getPackageManager().setComponentEnabledSetting(alias, componentState, PackageManager.DONT_KILL_APP);
+            return true;
+        }
+    };
+
+    private AlertDialog createRebootDialog() {
+        final Activity activity = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.app_name);
+        //builder.setIcon(android.R.drawable.ic_dialog_info);
+
+        // build view from layout
+        View dialogView = View.inflate(activity, R.layout.reboot_dialog, null);
+        final CheckBox neverShowAgainCheckBox = (CheckBox) dialogView.findViewById(R.id.reboot_dialog_checkbox);
+        builder.setView(dialogView);
+
+        builder.setPositiveButton(activity.getString(R.string.ok_understand), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // set preference to never show reboot dialog again if checkbox is checked
+                if (neverShowAgainCheckBox.isChecked()) {
+                    SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("pref_show_reboot_dialog", false);
+                    editor.apply();
+                }
+            }
+        });
+        return builder.create();
+    }
+}
